16e437be343ef8bd63133a93b308b1893c00563b
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2024-08-26 09:55:57 -0300

    add function get_distance_from_target

--- v1_shared_autonomy/utils/utilities.py
+++ v1_shared_autonomy/utils/utilities.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 
 def normalize_to_range(value, min_val, max_val, new_min, new_max, clip=False):
     """
@@ -32,6 +32,14 @@ def normalize_to_range(value, min_val, max_val, new_min, new_max, clip=False):
     else:
         return (new_max - new_min) / (max_val - min_val) * (value - max_val) + new_max
 
+def get_distance_from_target(robot_node, target_node):
+    robot_coordinates = robot_node.getField('translation').getSFVec3f()
+    target_coordinate = target_node.getField('translation').getSFVec3f()
+
+    dx = robot_coordinates[0] - target_coordinate[0]
+    dy = robot_coordinates[1] - target_coordinate[1]
+    distance_from_target = math.sqrt(dx * dx + dy * dy)
+    return distance_from_target
 
 def plot_data(data, x_label, y_label, plot_title, save=False, save_name=None):
     """
